b416236720c04c298db6f5cd7ec890eb83be1d4c
jisuyoun <nigayoun@gmail.com>
2020-06-10 17:14:42 +0900

    feature. pushstate와 popstate를 활용한 기본 라우팅 동작완성

---
+++ app.js
@@ -0,0 +1,79 @@
+
+// Util
+function getPath() {
+    return location.pathname;
+}
+
+function getCurrentPath(e) {
+    const {target} = e;
+    const listNode = e.target.closest("li");
+    if(!listNode) return;
+    if(target.nodeName === "A")  e.preventDefault();
+    const path = listNode.querySelector("a").getAttribute("href");
+    return path;
+}
+
+async function getInitData() {
+    const res = await fetch('/api/content.json');
+    return res.json();
+}
+
+async function getStates(path) {
+    switch (path) {
+        case "/":
+            return Promise.resolve(); 
+        case "/content":
+            return await getInitData();
+        default:
+            break;
+    }
+}
+
+// render
+function render(renderTarget, sHTML) {
+    renderTarget.innerHTML = sHTML;
+}
+
+// Push State
+function onLink() {
+    document.querySelector("nav ul").addEventListener("click", async (e) => {
+        const path  = getCurrentPath(e);
+        const states = await getStates(path);
+        history.pushState(states, '', path)
+    })
+}
+
+// Pop State
+function onRouter() {
+    window.addEventListener("popstate", ({state}) => {
+        const renderTargetWrap = document.querySelector(".content-wrap");
+        const targetView = getPath();
+        const contentViewHTML = viewMap[targetView](state?.content);
+        render(renderTargetWrap, contentViewHTML);
+    });
+}
+
+// View
+const viewMap = {
+    '/'() {
+        return `<h2>안녕하세요 코드스쿼드에 오셨네요!</h2>`
+    },
+    '/content'(data) {
+        if(!data) return;
+        return Object.values(data).reduce( (prev,next) => {
+            return (
+                prev + 
+                `<li> 
+                    <h3>${next.name}</h3>
+                    <div>${next.body}</div>
+                </li>`
+            )
+        },``);
+    }
+}
+
+// Initialize
+!(function(){
+    onLink();
+    onRouter()
+})();
